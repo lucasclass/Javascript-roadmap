fd50a5521ea31aa5df38beeba0a2d7d2e8dbaef2
Lucas Class <lucas.azevedo@instacarro.com>
2022-03-23 16:10:51 -0300

    correção espaçamento

--- Passager counter app/index.js
+++ Passager counter app/index.js
@@ -3,41 +3,29 @@ let countEl = document.getElementById("count-el") // Pegue o elemento count-el, 
 let count = 0
 
 function increment () {
-
     count += 1
     countEl.innerText = count
-
 }
 
-function save () {
-    
+function save () {    
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-
-    console.log("count:", count)
-    
 }
 
 let welcomeEl = document.getElementById("welcome-el")
-
 let nome = "Lucas Class"
 let saudação = "Bem vindo "
 
 welcomeEl.innerText = saudação + nome
-
 welcomeEl.innerText += " 🙌"
 
 // ----> Prática de variáveis
 
 let nome2 = "Lucas"
 let sobrenome = "Class"
-
 let nomeCompleto = nome2 + " " + sobrenome
-
-console.log(nomeCompleto)
-
 let myPoints = 3
 
 function add3Points(){
@@ -54,15 +42,11 @@ add3Points()
 remove1Points()
 remove1Points()
 
-
-console.log(myPoints)
-
-// Produto
+// ---> Produto
 
 let errorParagraph = document.getElementById("error")
 
 function comprar() {
-    console.log("Botão clicado")
     errorParagraph.textContent = "Deu ruim padrin, tenta de novo!"
 }
 
@@ -78,14 +62,17 @@ function soma(){
     let result = num1 + num2
     sumEl.textContent = "Resultado: " + result
 }
+
 function sub(){
     let result = num1 - num2
     sumEl.textContent = "Resultado: " + result
 }
+
 function div(){
     let result = num1 / num2
     sumEl.textContent = "Resultado: " + result
 }
+
 function multi(){
     let result = num1 * num2
     sumEl.textContent = "Resultado: " + result
